b8408ae7bb025736bb3125ec854a8b4b9d806e14
IkoAfianando <ikoafianando123@gmail.com>
2022-01-09 22:15:22 +0700

    Initial Commit

--- resources/views/data/data_warga.blade.php
+++ resources/views/data/data_warga.blade.php
@@ -42,15 +42,11 @@
                         @csrf
                         <div class="card-body">
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                <label for="exampleInputEmail1">Nama</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Nama Anda">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputFile">File input</label>
+                                <label for="exampleInputPassword1">Foto</label>
                                 <div class="input-group">
                                     <div class="custom-file">
                                         <input type="file" class="custom-file-input" id="exampleInputFile">
@@ -60,6 +56,9 @@
                                         <span class="input-group-text">Upload</span>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputFile">File input</label>
                             </div>
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
